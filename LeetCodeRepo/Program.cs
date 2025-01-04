a9fd7b7a22592f9145809ae9c828de870a4cbb99
cgcrochet <crochetcameron@gmail.com>
2025-01-03 19:12:34 -0600

    Implement RemoveElement

---
+++ LeetCodeRepo/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using LeetCodeRepo;
+
+/* RemoveElement solution and test case
+ * The solution returns the amount of numbers in the array that are not equal 
+ * to the provided value. It also modifies the nums array such that elements
+ * which are not equal to val are in the front of the array and in their
+ * original order
+ */
+int[] nums = [3, 2, 2, 3];
+int val = 3;
+
+int result = RemoveElement.Solution(nums, val);
+
+Console.WriteLine("result: " + result);
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.WriteLine(i + ": " + nums[i]);
+}
---
+++ LeetCodeRepo/RemoveElement.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeRepo
+{
+    public static class RemoveElement
+    {
+        public static int Solution(int[] nums, int val)
+        {
+            List<int> numsNotEqualToVal = new();
+
+            foreach (int num in nums)
+            {
+                if (num != val)
+                {
+                    numsNotEqualToVal.Add(num);
+                }
+            }
+
+            for (int i = 0; i < numsNotEqualToVal.Count(); i++)
+            {
+                nums[i] = numsNotEqualToVal[i];
+            }
+
+            return numsNotEqualToVal.Count();
+        }
+    }
+}
